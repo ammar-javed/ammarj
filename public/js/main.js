51776a896b8482e8c77c0b9e75cac4566c77c089
Ammar Javed <ammarj965@gmail.com>
2015-09-27 20:39:59 -0400

    finished projects page

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
-$(function(){
+$(
+  function(){
   'use strict';
+
   var options = {
     anchors: 'a',
     prefetch: false,
@@ -25,7 +27,102 @@ $(function(){
         $container.html($newContent);
 
       }
-    }
+    },
+    onAfter: function ($container, $newContent) {
+        if ($('.gallery').length) {
+
+          var pswpElement = document.querySelectorAll('.pswp')[0];
+
+          // build items array
+          var items = [
+              {
+                  src: '/static/kbbq/kbbq5.jpg',
+                  w: 820,
+                  h: 615
+              },
+              {
+                  src: '/static/kbbq/kbbq1.jpg',
+                  w: 758,
+                  h: 1010
+              },
+              {
+                  src: '/static/kbbq/kbbq2.jpg',
+                  w: 473,
+                  h: 841
+              },
+              {
+                  src: '/static/kbbq/kbbq3.jpg',
+                  w: 635,
+                  h: 1129
+              },
+              {
+                  src: '/static/kbbq/kbbq4.jpg',
+                  w: 532,
+                  h: 946
+              }
+          ];
+
+          // define options (if needed)
+          var options = {
+              // optionName: 'option value'
+              // for example:
+              index: 0 // start at first slide
+          };
+
+          // Initializes and opens PhotoSwipe
+          var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+
+          gallery.init();
+        }
+      }
   },
   smoothState = $('#main').smoothState(options).data('smoothState');
-});
+
+  if ($('.gallery').length) {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: '/static/kbbq/kbbq5.jpg',
+            w: 500,
+            h: 500
+        },
+        {
+            src: '/static/kbbq/kbbq1.jpg',
+            w: 500,
+            h: 500
+        },
+        {
+            src: '/static/kbbq/kbbq2.jpg',
+            w: 500,
+            h: 900
+        },
+        {
+            src: '/static/kbbq/kbbq3.jpg',
+            w: 500,
+            h: 900
+        },
+        {
+            src: '/static/kbbq/kbbq4.jpg',
+            w: 500,
+            h: 900
+        }
+    ];
+
+    // define options (if needed)
+    var options = {
+        // optionName: 'option value'
+        // for example:
+        index: 0 // start at first slide
+    };
+
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+
+    gallery.init();
+  }
+
+}
+
+);
--- public/js/projects.js
+++ public/js/projects.js
@@ -1,26 +1,45 @@
-var pswpElement = document.querySelectorAll('.pswp')[0];
+$( function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
 
-// build items array
-var items = [
-    {
-        src: 'http://localhost:3000/static/kbbq/kbbq1.jpg',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'http://localhost:3000/static/kbbq/kbbq2.jpg',
-        w: 1200,
-        h: 900
-    }
-];
+    // build items array
+    var items = [
+        {
+            src: '/static/kbbq/kbbq5.jpg',
+            w: 500,
+            h: 500
+        },
+        {
+            src: '/static/kbbq/kbbq1.jpg',
+            w: 500,
+            h: 500
+        },
+        {
+            src: '/static/kbbq/kbbq2.jpg',
+            w: 500,
+            h: 900
+        },
+        {
+            src: '/static/kbbq/kbbq3.jpg',
+            w: 500,
+            h: 900
+        },
+        {
+            src: '/static/kbbq/kbbq4.jpg',
+            w: 500,
+            h: 900
+        }
 
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-};
+    ];
 
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
+    // define options (if needed)
+    var options = {
+        // optionName: 'option value'
+        // for example:
+        index: 0 // start at first slide
+    };
+
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+
+    gallery.init();
+});
