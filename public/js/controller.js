8d7f3da21c54a793def220e9e37a4982ca3611a9
Ammar Javed <ammarj965@gmail.com>
2015-10-01 17:47:05 -0400

    Pulling out photoswipe, too heavy. Created bbq image thumbnails, started custom Img modal

--- public/js/controller.js
+++ public/js/controller.js
@@ -29,110 +29,37 @@ $(
       }
     },
     onAfter: function ($container, $newContent) {
-        if ($('.gallery').length) {
 
-          var pswpElement = document.querySelectorAll('.pswp')[0];
+      if ($('.projects').length) {
 
-          // build items array
-          var items = [
-              {
-                  src: '/static/kbbq/kbbq5.jpg',
-                  w: 820,
-                  h: 615
-              },
-              {
-                  src: '/static/kbbq/kbbq1.jpg',
-                  w: 758,
-                  h: 1010
-              },
-              {
-                  src: '/static/kbbq/kbbq2.jpg',
-                  w: 473,
-                  h: 841
-              },
-              {
-                  src: '/static/kbbq/kbbq3.jpg',
-                  w: 635,
-                  h: 1129
-              },
-              {
-                  src: '/static/kbbq/kbbq4.jpg',
-                  w: 532,
-                  h: 946
-              },
-              {
-                  src: '/static/kbbq/kbbq6.jpg',
-                  w: 532,
-                  h: 946
-              },
-              {
-                  src: '/static/kbbq/kbbq7.jpg',
-                  w: 532,
-                  h: 946
-              }
-          ];
+        $(".bbq-gal").on("click", "a", function(e) {
+            $(".modal").show();
+        });
 
-          // define options (if needed)
-          var options = {
-              // optionName: 'option value'
-              // for example:
-              index: 0 // start at first slide
-          };
+        $(document).mouseup( function (e) {
+          var img = $("p");
 
-          // Initializes and opens PhotoSwipe
-          var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+          if (!img.is(e.target) && img.has(e.target).length === 0) {
+            $(".modal").hide();
+          }
+        });
 
-          gallery.init();
-        }
       }
+
+    }
   },
   smoothState = $('#main').smoothState(options).data('smoothState');
 
   if ($('.gallery').length) {
-    var pswpElement = document.querySelectorAll('.pswp')[0];
 
-    // build items array
-    var items = [
-        {
-            src: '/static/kbbq/kbbq5.jpg',
-            w: 500,
-            h: 500
-        },
-        {
-            src: '/static/kbbq/kbbq1.jpg',
-            w: 500,
-            h: 500
-        },
-        {
-            src: '/static/kbbq/kbbq2.jpg',
-            w: 500,
-            h: 900
-        },
-        {
-            src: '/static/kbbq/kbbq3.jpg',
-            w: 500,
-            h: 900
-        },
-        {
-            src: '/static/kbbq/kbbq4.jpg',
-            w: 500,
-            h: 900
-        }
-    ];
+    $(".bbq-gal").on("click", "a", function(e) {
+        $(".modal").show();
+    });
 
-    // define options (if needed)
-    var options = {
-        // optionName: 'option value'
-        // for example:
-        index: 0 // start at first slide
-    };
-
-    // Initializes and opens PhotoSwipe
-    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-
-    gallery.init();
+    $(".modal").click(function(e) {
+      $(".modal").hide();
+    });
   }
 
 }
-
 );
